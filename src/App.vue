2035c71c79ce555f9d519efe7a12657ef14b8d2c
Ahmed El Moden <cresuso@gmail.com>
2020-10-07 16:14:58 +0200

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input :value="formatDuration(substep.estimatedDuration)"
+      @input="substep.estimatedDuration = durationToMinutes($event.target.value)"
+      class="form-control html-duration-picker" data-hide-seconds />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable no-debugger */
+import * as HtmlDurationPicker from 'html-duration-picker'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data()
+  {
+    return {
+      substep: {
+        estimatedDuration: 100
+      }
+    };
+  },
+  mounted()
+  {
+    HtmlDurationPicker.init();
+  },
+  methods:
+  {
+    formatDuration(duration)
+    {
+      if (typeof duration==='number' && (duration%1)===0)
+      {
+        const hours = parseInt(duration / 60);
+        const minutes = duration % 60;
+        const result = hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0');
+        debugger
+        return result;
+      }
+    },
+
+    durationToMinutes(duration)
+    {
+      const split = duration.split(':');
+      const minutes = Number(split[1]);
+      const hoursToMinutes = Number(split[0]) * 60;
+      return hoursToMinutes + minutes;
+    },
+
   }
 }
 </script>
